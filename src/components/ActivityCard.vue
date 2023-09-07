0196160fdf0c2d244df8460515eee5ec494d2e82
Robin Littiere <zauts95@gmail.com>
2023-09-07 16:32:46 +0200

    added activity cards component

---
+++ src/components/ActivityCard.vue
@@ -0,0 +1,29 @@
+<template lang="">
+  <div class="activity_container">
+    <div class="activity_title">{{ title }}</div>
+    <div class="activity_content">
+      <div class="activity_icon"></div>
+      <div class="activity_data">{{ content }}</div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    title: String,
+    content: String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.activity_container {
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  min-height: 20vh;
+  margin: 8px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,20 @@
 import Home from "@/components/Home.vue";
 import HeaderAdmin from "@/components/HeaderAdmin.vue";
 import Sidebar from "@/components/Sidebar.vue";
+import ActivityCard from "@/components/ActivityCard.vue";
 </script>
 
 <template>
   <HeaderAdmin />
   <div class="view_container">
     <Sidebar />
-    <main class="content"></main>
+    <main class="content">
+      <div class="activity_grid">
+        <ActivityCard title="Nombre de machines monitoré" content="32" />
+        <ActivityCard title="Nombre de machines pingé" content="14" />
+        <ActivityCard title="Alertes machines inactives" content="2" />
+      </div>
+    </main>
   </div>
 </template>
 
@@ -20,7 +27,15 @@ import Sidebar from "@/components/Sidebar.vue";
 }
 
 .content {
+  padding: 2em;
   flex: 1;
   background-color: var(--color-background-mute);
+  display: flex;
+  flex-direction: column;
+}
+
+.activity_grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
 }
 </style>
